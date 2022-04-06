33fffa4b0d2484377a7cdb8ea87abf320f293f30
Nauris Brunins <naurisb@mikrotik.com>
2022-04-06 12:45:56 +0300

    basic pin test

---
+++ src/dwm1001.h
@@ -0,0 +1,15 @@
+#ifndef __DWM1001_h__
+#define __DWM1001_h__
+
+#define ST_LD1	30
+#define ST_LD2	31
+#define ST_LD3	22
+#define ST_LD4	14
+
+#define LEDS	{ ST_LD1, ST_LD2, ST_LD3, ST_LD4 }
+
+#define BT_WAKE	2
+
+#define BTNS	{ BT_WAKE }
+
+#endif
